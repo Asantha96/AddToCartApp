a0a1b503f7ced3f5f5de40bbcab0a80167f46636
Asantha <asanthadhananjaya@gmail.com>
2023-11-06 18:33:38 +0530

    design the basic structure of the app

---
+++ index.js
@@ -0,0 +1,8 @@
+
+const inputFieldElement = document.getElementById("input-field");
+const addButtonElement = document.getElementById("add-button");
+
+addButtonElement.addEventListener("click", function(){
+  let inputValue = inputFieldElement.value;
+  console.log(inputValue);
+});
