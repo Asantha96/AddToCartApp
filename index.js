9e5e79db9408d99ef0ee400d8edc698a9e4465e7
Asantha <asanthadhananjaya@gmail.com>
2023-11-07 08:04:43 +0530

    completed app and deployed on netlify

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
   databaseURL : "https://todoapp-18b00-default-rtdb.firebaseio.com/"
@@ -10,22 +9,54 @@ const database = getDatabase(app);
 const shoppingListInDB = ref(database, "shoppingList");
 
 
+
 const inputFieldElement = document.getElementById("input-field");
 const addButtonElement = document.getElementById("add-button");
 const shoppingListElement = document.getElementById("shopping-list");
 
+//this function is always running when there is a change to the database
+onValue(shoppingListInDB, function(snapshot){
+
+  if(snapshot.exists()){
+    let shoppingListArray = Object.entries(snapshot.val());
+    //console.log(shoppingListArray);
+    console.log(snapshot.val())
+    clearShoppingListElement();
+    shoppingListArray.forEach(array=> {//this array is contain only 2 things, id and value
+      let currentItem = array;
+      addNewListItem(currentItem);
+    });
+  }else{
+    shoppingListElement.innerHTML = "No items here..."
+  }
+  
+})
+
+
 addButtonElement.addEventListener("click", function(){
   let inputValue = inputFieldElement.value;
-
-  let newListItem = document.createElement("li");
-  newListItem.textContent = inputValue;
-  shoppingListElement.appendChild(newListItem);
-
-  clearInputField;
-
-  push(shoppingListInDB, inputValue);
+  if(inputValue != ""){
+    clearInputField();
+    push(shoppingListInDB, inputValue);
+  }
 });
 
 function clearInputField(){
   inputFieldElement.value = "";
+}
+
+function addNewListItem(currentItemArray){
+  let itemId = currentItemArray[0];
+  let itemValue = currentItemArray[1];
+  let newListItem = document.createElement("li");
+  newListItem.textContent = itemValue;//shoppingListElement.innerHTML += `<li>${inputValue}</li>`
+  newListItem.addEventListener("click", function(){
+    let exactLocationOfItemInDB = ref(database, `shoppingList/${itemId}`);
+    remove(exactLocationOfItemInDB);
+  });
+  shoppingListElement.appendChild(newListItem);
+}
+
+function clearShoppingListElement(){
+  shoppingListElement.innerHTML = "";
 }
